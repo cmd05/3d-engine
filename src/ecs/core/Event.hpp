140f594db2be475ebf5994d8f16577dc358759e0
cmd05 <splendid.snippet670@slmail.me>
2024-08-17 20:12:05 +0530

    Use ParamId for event parameters

--- src/ecs/core/Event.hpp
+++ src/ecs/core/Event.hpp
@@ -11,24 +11,26 @@ public:
     explicit Event(EventId type);
 
     template<typename T>
-    void set_param(EventId id, T value);
+    void set_param(ParamId id, T value);
     
     template<typename T>
-    T get_param(EventId id);
+    T get_param(ParamId id);
 
     EventId get_type() const;
 
 private:
     EventId m_type{};
-    std::unordered_map<EventId, std::any> m_data{};
+    std::unordered_map<ParamId, std::any> m_data{};
 };
 
 template<typename T>
-void Event::set_param(EventId id, T value) {
+void Event::set_param(ParamId id, T value) {
     m_data[id] = value;
 }
 
 template<typename T>
-T Event::get_param(EventId id) {
+T Event::get_param(ParamId id) {
+    // get specific parameter from event
+    // cast it to type specified by caller (using std::any_cast<T>) before returning
     return std::any_cast<T>(m_data[id]);
 }
