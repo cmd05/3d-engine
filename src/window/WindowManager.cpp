a0999cfa8feeb43f8af815e909c0045f344a9b98
cmd05 <splendid.snippet670@slmail.me>
2024-08-17 16:46:11 +0530

    perfect camera movement

--- src/window/WindowManager.cpp
+++ src/window/WindowManager.cpp
@@ -50,41 +50,70 @@ void WindowManager::ProcessEvents()
 {
     glfwPollEvents();
 
-    // bool buttonStateChanged = true;
+    // bool buttonStateChanged = false;
 
     if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     {
         gCoordinator.send_event(Events::Window::QUIT);
+        // buttonStateChanged = true;
     }
-    else if (glfwGetKey(mWindow, GLFW_KEY_W) == GLFW_PRESS)
+    
+    if (glfwGetKey(mWindow, GLFW_KEY_W) == GLFW_PRESS)
     {
         mButtons.set(static_cast<std::size_t>(InputButtons::W));
+        // buttonStateChanged = true;
+    } else {
+        mButtons.reset(static_cast<std::size_t>(InputButtons::W));
     }
-    else if (glfwGetKey(mWindow, GLFW_KEY_A) == GLFW_PRESS)
+
+    if (glfwGetKey(mWindow, GLFW_KEY_A) == GLFW_PRESS)
     {
         mButtons.set(static_cast<std::size_t>(InputButtons::A));
+        // buttonStateChanged = true;
+    } else {
+        mButtons.reset(static_cast<std::size_t>(InputButtons::A));
     }
-    else if (glfwGetKey(mWindow, GLFW_KEY_S) == GLFW_PRESS)
+
+    if (glfwGetKey(mWindow, GLFW_KEY_S) == GLFW_PRESS)
     {
         mButtons.set(static_cast<std::size_t>(InputButtons::S));
+        // buttonStateChanged = true;
+    } else {
+        mButtons.reset(static_cast<std::size_t>(InputButtons::S));
     }
-    else if (glfwGetKey(mWindow, GLFW_KEY_D) == GLFW_PRESS)
+    
+    if (glfwGetKey(mWindow, GLFW_KEY_D) == GLFW_PRESS)
     {
         mButtons.set(static_cast<std::size_t>(InputButtons::D));
+        // buttonStateChanged = true;
+    } else {
+        mButtons.reset(static_cast<std::size_t>(InputButtons::D));
     }
-    else if (glfwGetKey(mWindow, GLFW_KEY_Q) == GLFW_PRESS)
+    
+    if (glfwGetKey(mWindow, GLFW_KEY_Q) == GLFW_PRESS)
     {
         mButtons.set(static_cast<std::size_t>(InputButtons::Q));
+        // buttonStateChanged = true;
+    } else {
+        mButtons.reset(static_cast<std::size_t>(InputButtons::Q));
     }
-    else if (glfwGetKey(mWindow, GLFW_KEY_E) == GLFW_PRESS)
+
+    if (glfwGetKey(mWindow, GLFW_KEY_E) == GLFW_PRESS)
     {
         mButtons.set(static_cast<std::size_t>(InputButtons::E));
+        // buttonStateChanged = true;
+    } else {
+        mButtons.reset(static_cast<std::size_t>(InputButtons::E));
     }
-    else
-    {
-        mButtons.reset();
-        // buttonStateChanged = false;
-    }
+
+    // else
+    // {
+    //     mButtons.reset();
+    //     // buttonStateChanged = false;
+    // }
+
+    // if(!buttonStateChanged)
+    //     mButtons.reset();
 
     // if (buttonStateChanged)
     // {
