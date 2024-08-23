7a47aec408b3ddaaff840ad961773fb5bec911bf
cmd05 <splendid.snippet670@slmail.me>
2024-08-23 20:02:15 +0530

    implement sparse sets

--- src/ecs/core/EntityManager.hpp
+++ src/ecs/core/EntityManager.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <queue>
-#include <map>
+#include <array>
+#include <vector>
 
 #include <ecs/core/Types.hpp>
 
@@ -11,12 +12,19 @@ public:
 
     Entity create_entity();
     void destroy_entity(Entity entity);
+
     void set_signature(Entity entity, Signature signature);
     Signature get_signature(Entity entity);
+
+    void clear();
 
 private:
     std::queue<Entity> destroyed_entities{};
 
-    std::map<Entity, Signature> m_signatures{};
+    // sparse set: map from entities to signatures
+    std::array<entity_count_size_type, MAX_ENTITIES> m_sparse_array;
+    std::vector<Entity> m_dense_entities;
+    std::vector<Signature> m_dense_signatures;
+
     Entity last_entity = 0;
 };
