6756ebd5c600f59e5abcc4ba3be3cb9baee0768d
cmd05 <splendid.snippet670@slmail.me>
2024-08-23 22:31:59 +0530

    implement stateless systems

--- src/ecs/core/System.hpp
+++ src/ecs/core/System.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <set>
-
 #include <ecs/core/Types.hpp>
 
 class Coordinator;
@@ -11,14 +9,7 @@ class Coordinator;
 class System
 {
 public:
-    // Removing entity from std::set is faster than a list
-    // inserting in std::set for existing element does nothing.
-    // If trying to erase when it doesn’t exist, it does nothing
-
-    // All entities with a certain signature
     System(Coordinator& coordinator) : ref_gcoordinator(coordinator) {}
-    
-    std::set<Entity> m_entities;
 protected:
     Coordinator& ref_gcoordinator;
 };
