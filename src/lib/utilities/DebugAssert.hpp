1fe158ed60468f82149c4454b5121abedd60fafe
cmd05 <splendid.snippet670@slmail.me>
2024-08-23 21:41:52 +0530

    provide a custom assert with runtime debug messages

--- src/lib/utilities/DebugAssert.hpp
+++ src/lib/utilities/DebugAssert.hpp
@@ -1,12 +1,14 @@
 #ifndef NDEBUG
-#   define ASSERT(condition, message) \
-    do { \
-        if (! (condition)) { \
-            std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
-                      << " line " << __LINE__ << ": " << message << std::endl; \
-            std::terminate(); \
-        } \
-    } while (false)
+#include <iostream>
+#include <exception>
+#define ASSERT(condition, message) \
+        do { \
+            if (!(condition)) { \
+                std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
+                            << " line " << __LINE__ << ": " << message << std::endl; \
+                std::terminate(); \
+            } \
+        } while (false)
 #else
-#   define ASSERT(condition, message) do { } while (false)
+#define ASSERT(condition, message) do { } while (false)
 #endif
