4a4daddfca82c8d5585ddb916733a9a6a68cbfcd
cmd05 <splendid.snippet670@slmail.me>
2024-08-23 19:50:45 +0530

    implement sparse sets

--- src/ecs/core/ComponentArray.hpp
+++ src/ecs/core/ComponentArray.hpp
@@ -1,10 +1,8 @@
 #pragma once
 
-#include <utility>
 #include <cassert>
-#include <unordered_map>
 #include <array>
-// #include <vector>
+#include <vector>
 
 #include <lib/simple-vector/SimpleVector.hpp>
 
@@ -39,8 +37,12 @@ public:
     
     void entity_destroyed(Entity entity);
     
-    void clear();
+    bool has_component(Entity entity) const;
+    T* get_component(Entity entity) const;
+    T& get_data(Entity entity);
     
+    void clear();
+
     entity_count_size_type size() const;
 
     // provide begin and end iterators
@@ -50,9 +52,13 @@ public:
     vector_entity_const_iterator cbegin() const { return m_dense_entities.cbegin(); }
     vector_entity_const_iterator cend() const { return m_dense_entities.cend(); }
 private:
-    std::array<Entity, MAX_ENTITIES> m_sparse_array;
-    SimpleVector<std::pair<Entity, T>, entity_count_size_type> m_component_vector;
-    // std::vector<std::pair<Entity, T>> m_component_vector;
+    // `Entity` to `T` sparse set
+    std::array<entity_count_size_type, MAX_ENTITIES> m_sparse_array;
+    std::vector<Entity> m_dense_entities;
+    std::vector<T> m_component_vector;
+
+    // SimpleVector<T, entity_count_size_type> m_component_vector;
+    // SimpleVector<T, entity_count_size_type> m_dense_entities;
 };
 
 template<typename T>
@@ -67,30 +73,39 @@ entity_count_size_type ComponentArray<T>::size() const {
 
 template<typename T>
 void ComponentArray<T>::insert_data(Entity entity, T component) {
-    assert(!(m_component_vector.capacity() >= MAX_ENTITIES && m_component_vector.size() == MAX_ENTITIES) && "component array size exceeded MAX_ENTITIES");
+    assert(!(m_component_vector.capacity() >= MAX_ENTITIES && m_component_vector.size() == MAX_ENTITIES) && "Component Array size exceeded MAX_ENTITIES");
 
     assert(m_sparse_array[entity] == NO_COMPONENT_MARKER && "Component added to same entity more than once.");
     
-    // put new entry
+    // insert new component
     entity_count_size_type new_index = m_component_vector.size();
+
     m_sparse_array[entity] = new_index;
-    m_component_vector.push_back(std::make_pair(entity, component));
+    m_dense_entities.push_back(entity);
+    m_component_vector.push_back(component);
 }
 
 template<typename T>
 void ComponentArray<T>::remove_data(Entity entity) {
-    assert(m_sparse_array[entity] != NO_COMPONENT_MARKER && "Component does not exist for given entity");
-
-    // copy element at the end into deleted element's place to maintain density
     entity_count_size_type index_removed_entity = m_sparse_array[entity];
-    entity_count_size_type index_last_elem = m_component_vector.size() - 1;
 
-    // update sparse set
-    Entity entity_last_elem = m_component_vector[index_last_elem].first;
+    assert(index_removed_entity != NO_COMPONENT_MARKER && "Component does not exist for given entity");
+
+    // copy the last element into the removed element's place to maintain density
+
+    entity_count_size_type index_last_elem = m_component_vector.size() - 1;
+    Entity entity_last_elem = m_dense_entities[index_last_elem];
+
+    // move last element in place of removed element 
     m_sparse_array[entity_last_elem] = index_removed_entity;
+    m_dense_entities[index_removed_entity] = m_dense_entities[index_last_elem];
     m_component_vector[index_removed_entity] = m_component_vector[index_last_elem];
 
+    // remove entity
     m_sparse_array[entity] = NO_COMPONENT_MARKER;
+
+    // remove the last element
+    m_dense_entities.pop_back();
     m_component_vector.pop_back();
 }
 
@@ -100,9 +115,21 @@ T& ComponentArray<T>::get_data(Entity entity) {
 
     assert(index != NO_COMPONENT_MARKER && "Retrieving non existent component");
     
-    T& component = m_component_vector[index].second;
+    T& component = m_component_vector[index];
 
     return component;
+}
+
+template<typename T>
+T* ComponentArray<T>::get_component(Entity entity) const {
+    entity_count_size_type index = m_sparse_array[entity];
+
+    return index != NO_COMPONENT_MARKER ? &m_component_vector[index] : nullptr;
+}
+
+template<typename T>
+bool ComponentArray<T>::has_component(Entity entity) const {
+    return m_sparse_array[entity] != NO_COMPONENT_MARKER;
 }
 
 template<typename T>
@@ -113,6 +140,9 @@ void ComponentArray<T>::entity_destroyed(Entity entity) {
 
 template<typename T>
 void ComponentArray<T>::clear() {
-    m_sparse_array.fill(NO_COMPONENT_MARKER);
+    for(Entity entity : m_dense_entities)
+        m_sparse_array[entity] = NO_COMPONENT_MARKER; // saves iterations by resetting only existing entities
+    
+    m_dense_entities.clear();
     m_component_vector.clear();
 }
