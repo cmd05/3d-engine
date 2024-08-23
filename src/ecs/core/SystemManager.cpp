295656c657522fba054f233830357b427afbd726
cmd05 <splendid.snippet670@slmail.me>
2024-08-23 21:40:18 +0530

    TO FIX: comment insertion of entitities into system::m_entities

--- src/ecs/core/SystemManager.cpp
+++ src/ecs/core/SystemManager.cpp
@@ -5,25 +5,25 @@
 void SystemManager::entity_destroyed(Entity entity) {
     // erase a destroyed entity from all system lists
     // system->m_entities is a set, so no check need
-    for(const auto& pair : m_systems) {
-        const auto& system = pair.second;
-        system->m_entities.erase(entity);
-    }
+    // for(const auto& pair : m_systems) {
+    //     const auto& system = pair.second;
+    //     system->m_entities.erase(entity);
+    // }
 }
 
 void SystemManager::entity_signature_changed(Entity entity, Signature entity_new_signature) {
     // notify each system that an entity's signature changed
-    for(const auto& pair : m_systems) {
-        const auto& type = pair.first;
-        const auto& system = pair.second;
-        const auto& system_signature = m_signatures[type];
+    // for(const auto& pair : m_systems) {
+    //     const auto& type = pair.first;
+    //     const auto& system = pair.second;
+    //     const auto& system_signature = m_signatures[type];
 
-        // entity signature matches system signature - insert into set
-        if((entity_new_signature & system_signature) == system_signature) {
-            system->m_entities.insert(entity);
-        } else {
-            // entity signature does not match system signature - erase from set
-            system->m_entities.erase(entity);
-        }
-    }
+    //     // entity signature matches system signature - insert into set
+    //     if((entity_new_signature & system_signature) == system_signature) {
+    //         system->m_entities.insert(entity);
+    //     } else {
+    //         // entity signature does not match system signature - erase from set
+    //         system->m_entities.erase(entity);
+    //     }
+    // }
 }
