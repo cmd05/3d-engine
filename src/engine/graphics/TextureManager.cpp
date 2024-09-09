174fa88dbd3d6ae2bf96c868331fc419c994b7c4
cmd05 <splendid.snippet670@slmail.me>
2024-09-09 13:56:55 +0530

    1. provide texture loading 2. provide cubemap loading

---
+++ src/engine/graphics/TextureManager.cpp
@@ -0,0 +1,142 @@
+#include <glad/glad.h> 
+#include "../../../includes/stb/stb_image.h"
+
+#include <array>
+#include <unordered_map>
+#include <string>
+
+#include <engine/graphics/Shader.hpp>
+#include <engine/graphics/simple-objects/SkyboxVertices.hpp>
+#include <lib/utilities/DebugAssert.hpp>
+#include <engine/graphics/TextureManager.hpp>
+
+TextureManager::TextureManager() {
+    // setup cubemap vao
+    unsigned int cubemap_vbo;
+
+    glGenVertexArrays(1, &m_cubemap_vao);
+    glGenBuffers(1, &cubemap_vbo);
+    
+    glBindVertexArray(m_cubemap_vao);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, cubemap_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(SKYBOX_VERTICES), &SKYBOX_VERTICES, GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+
+    glBindVertexArray(0);
+}
+
+// if same texture path already exists return the texture id
+// else create new texture and return its id 
+unsigned int TextureManager::texture_from_file(std::string file_path, bool gamma) {
+    // FIX: allow a `bool flip` parameter to the function. ex: flip backpack.obj texture
+    // stbi_set_flip_vertically_on_load(true);
+    
+    // check if texture exists
+    auto it = m_loaded_textures.begin();
+    if((it = m_loaded_textures.find(file_path)) != m_loaded_textures.end())
+        return it->second;
+    
+    // create a new texture
+    unsigned int texture_id;
+    glGenTextures(1, &texture_id);
+
+    int width, height, num_components;
+    unsigned char *img_data = stbi_load(file_path.c_str(), &width, &height, &num_components, 0);
+    
+    if (img_data) {
+        GLenum format;
+        if (num_components == 1)
+            format = GL_RED;
+        else if (num_components == 3)
+            format = GL_RGB;
+        else if (num_components == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, texture_id);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, img_data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    } else {
+        ASSERT(false, "Texture data could not be loaded: " << file_path);
+    }
+    
+    stbi_image_free(img_data);
+
+    glBindTexture(GL_TEXTURE_2D, 0); // reset bound texture
+    m_loaded_textures[file_path] = texture_id; // add to loaded textures
+
+    return texture_id;
+}
+
+cubemaps_interface_type TextureManager::load_cubemaps(std::unordered_map<std::string, CubemapFaces> cubemaps) {
+    cubemaps_interface_type loaded_cubemaps;
+
+    for(auto [name, cubemap] : cubemaps)
+        loaded_cubemaps[name] = add_cubemap(cubemap);
+
+    return loaded_cubemaps;
+}
+
+unsigned int TextureManager::add_cubemap(CubemapFaces faces) {
+    unsigned int texture_id;
+    glGenTextures(1, &texture_id);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, texture_id);
+
+    // order as required by GL_TEXTURE_CUBE_MAP_POSITIVE_X
+    cubemap_array<std::string> faces_ordered = {faces.right, faces.left, faces.top, faces.bottom, faces.front, faces.back};
+
+    int width, height, num_components;
+
+    for(std::size_t i = 0; i < faces_ordered.size(); i++) {
+        unsigned char* img_data = stbi_load(faces_ordered[i].c_str(), &width, &height, &num_components, 0);
+            
+        if (img_data)
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data);
+        else
+            ASSERT(false, "Cubemap texture failed to load at path: " << faces_ordered[i]);
+        
+        stbi_image_free(img_data);
+    }
+
+    // set cubemap properties
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    m_cubemaps.push_back(texture_id);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+
+    return texture_id;
+}
+
+void TextureManager::draw_cubemap(unsigned int cubemap_id, Shader& cubemap_shader, glm::mat4 view_matrix, glm::mat4 projection_matrix) {
+    // cubemap_shader.activate();
+    // glUniform1i(glGetUniformLocation(shader.get_id(), "skybox"), 0);
+    
+    glDepthFunc(GL_LEQUAL); // change depth function so depth test passes when values are equal to depth buffer's content
+    
+    cubemap_shader.activate();
+    // FIX: fix setting texture units and other constant uniforms before render loop (in constructor / rendersystem::init())
+    glUniform1i(glGetUniformLocation(cubemap_shader.get_id(), "skybox"), 0);
+    
+    glm::mat4 view = glm::mat4(glm::mat3(view_matrix)); // remove translation from the view matrix
+    cubemap_shader.set_uniform<glm::mat4>("view", view);
+    cubemap_shader.set_uniform<glm::mat4>("projection", projection_matrix);
+    
+    // skybox cube
+    glBindVertexArray(m_cubemap_vao);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap_id);
+    glDrawArrays(GL_TRIANGLES, 0, SKYBOX_NUM_VERTICES);
+    glBindVertexArray(0);
+    glDepthFunc(GL_LESS); // set depth function back to default
+}
---
+++ src/engine/graphics/TextureManager.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <unordered_map>
+#include <array>
+#include <vector>
+#include <string>
+
+#include <engine/graphics/Shader.hpp>
+
+template<typename T>
+using cubemap_array = std::array<T, 6>;
+
+using cubemaps_interface_type = std::unordered_map<std::string, unsigned int>;
+
+struct CubemapFaces {
+    std::string right, left, top, bottom, front, back;
+};
+
+class TextureManager {
+public:
+    TextureManager();
+    unsigned int texture_from_file(std::string file_path, bool gamma = false);
+
+    cubemaps_interface_type load_cubemaps(std::unordered_map<std::string, CubemapFaces> cubemaps);
+    unsigned int add_cubemap(CubemapFaces faces);
+    // FIX: dirty workaround right now for matrices. take Camera& as a reference, for camera class
+    void draw_cubemap(unsigned int cubemap_id, Shader& cubemap_shader, glm::mat4 view_matrix, glm::mat4 projection_matrix);
+private:
+    // full texture path, texture id
+    std::unordered_map<std::string, unsigned int> m_loaded_textures;
+    
+    std::vector<unsigned int> m_cubemaps;
+    unsigned int m_cubemap_vao;
+};
