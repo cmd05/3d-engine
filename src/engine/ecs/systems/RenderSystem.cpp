3f24e824eef34cb2e620c0c9fd0f7e7b63644697
cmd05 <splendid.snippet670@slmail.me>
2024-09-09 13:51:42 +0530

    add logic for cubemap and model rendering logic

--- src/engine/ecs/systems/RenderSystem.cpp
+++ src/engine/ecs/systems/RenderSystem.cpp
@@ -9,141 +9,100 @@
 #include <engine/ecs/core/Scene.hpp>
 #include <engine/ecs/core/SceneView.hpp>
 
+#include <engine/ecs/components/Cubemap.hpp>
 #include <engine/ecs/components/Camera.hpp>
 #include <engine/ecs/components/Renderable.hpp>
 #include <engine/ecs/components/Transform.hpp>
+#include <engine/ecs/components/Model.hpp>
 
 #include <engine/graphics/Shader.hpp>
 
-void RenderSystem::init(Entity camera)
-{
-    // add window resize listener
-    ref_scene.add_event_listener(METHOD_LISTENER(Events::Window::RESIZED, RenderSystem::window_size_listener));
-    // create shader uniqueptr
-    shader = std::make_unique<Shader>(std::string(FS_SHADERS_DIR) + "vertex.vs", std::string(FS_SHADERS_DIR) + "fragment.fs");
-
-    m_camera = camera;
-
-    // save cube data in m_vao and vbo's
-    std::vector<glm::vec3> vertices =
-    {
-        glm::vec3(-0.5f, -0.5f, -0.5f),
-        glm::vec3(0.5f, -0.5f, -0.5f),
-        glm::vec3(0.5f, 0.5f, -0.5f),
-        glm::vec3(0.5f, 0.5f, -0.5f),
-        glm::vec3(-0.5f, 0.5f, -0.5f),
-        glm::vec3(-0.5f, -0.5f, -0.5f),
-        glm::vec3(-0.5f, -0.5f, 0.5),
-        glm::vec3(0.5f, -0.5f, 0.5),
-        glm::vec3(0.5f, 0.5f, 0.5),
-        glm::vec3(0.5f, 0.5f, 0.5),
-        glm::vec3(-0.5f, 0.5f, 0.5),
-        glm::vec3(-0.5f, -0.5f, 0.5),
-        glm::vec3(-0.5f, 0.5f, 0.5f),
-        glm::vec3(-0.5f, 0.5f, -0.5f),
-        glm::vec3(-0.5f, -0.5f, -0.5f),
-        glm::vec3(-0.5f, -0.5f, -0.5f),
-        glm::vec3(-0.5f, -0.5f, 0.5f),
-        glm::vec3(-0.5f, 0.5f, 0.5f),
-        glm::vec3(0.5f, 0.5f, 0.5),
-        glm::vec3(0.5f, 0.5f, -0.5),
-        glm::vec3(0.5f, -0.5f, -0.5),
-        glm::vec3(0.5f, -0.5f, -0.5),
-        glm::vec3(0.5f, -0.5f, 0.5),
-        glm::vec3(0.5f, 0.5f, 0.5),
-        glm::vec3(-0.5f, -0.5f, -0.5f),
-        glm::vec3(0.5f, -0.5f, -0.5f),
-        glm::vec3(0.5f, -0.5f, 0.5f),
-        glm::vec3(0.5f, -0.5f, 0.5f),
-        glm::vec3(-0.5f, -0.5f, 0.5f),
-        glm::vec3(-0.5f, -0.5f, -0.5f),
-        glm::vec3(-0.5f, 0.5f, -0.5),
-        glm::vec3(0.5f, 0.5f, -0.5),
-        glm::vec3(0.5f, 0.5f, 0.5),
-        glm::vec3(0.5f, 0.5f, 0.5),
-        glm::vec3(-0.5f, 0.5f, 0.5),
-        glm::vec3(-0.5f, 0.5f, -0.5)
-    };
-
-    std::vector<glm::vec3> normals =
-    {
-        glm::vec3(0.0f, 0.0f, -1.0f),
-        glm::vec3(0.0f, 0.0f, -1.0f),
-        glm::vec3(0.0f, 0.0f, -1.0f),
-        glm::vec3(0.0f, 0.0f, -1.0f),
-        glm::vec3(0.0f, 0.0f, -1.0f),
-        glm::vec3(0.0f, 0.0f, -1.0f),
-        glm::vec3(0.0f, 0.0f, 1.0f),
-        glm::vec3(0.0f, 0.0f, 1.0f),
-        glm::vec3(0.0f, 0.0f, 1.0f),
-        glm::vec3(0.0f, 0.0f, 1.0f),
-        glm::vec3(0.0f, 0.0f, 1.0f),
-        glm::vec3(0.0f, 0.0f, 1.0f),
-        glm::vec3(-1.0f, 0.0f, 0.0f),
-        glm::vec3(-1.0f, 0.0f, 0.0f),
-        glm::vec3(-1.0f, 0.0f, 0.0f),
-        glm::vec3(-1.0f, 0.0f, 0.0f),
-        glm::vec3(-1.0f, 0.0f, 0.0f),
-        glm::vec3(-1.0f, 0.0f, 0.0f),
-        glm::vec3(1.0f, 0.0f, 0.0f),
-        glm::vec3(1.0f, 0.0f, 0.0f),
-        glm::vec3(1.0f, 0.0f, 0.0f),
-        glm::vec3(1.0f, 0.0f, 0.0f),
-        glm::vec3(1.0f, 0.0f, 0.0f),
-        glm::vec3(1.0f, 0.0f, 0.0f),
-        glm::vec3(0.0f, -1.0f, 0.0f),
-        glm::vec3(0.0f, -1.0f, 0.0f),
-        glm::vec3(0.0f, -1.0f, 0.0f),
-        glm::vec3(0.0f, -1.0f, 0.0f),
-        glm::vec3(0.0f, -1.0f, 0.0f),
-        glm::vec3(0.0f, -1.0f, 0.0f),
-        glm::vec3(0.0f, 1.0f, 0.0f),
-        glm::vec3(0.0f, 1.0f, 0.0f),
-        glm::vec3(0.0f, 1.0f, 0.0f),
-        glm::vec3(0.0f, 1.0f, 0.0f),
-        glm::vec3(0.0f, 1.0f, 0.0f),
-        glm::vec3(0.0f, 1.0f, 0.0)
-    };
-
-    // use m_vao
-    glGenVertexArrays(1, &m_vao);
-    glBindVertexArray(m_vao);
-
-    // vertices
-    glGenBuffers(1, &m_vbo_vertices);
-    glBindBuffer(GL_ARRAY_BUFFER, m_vbo_vertices);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * vertices.size(), vertices.data(), GL_STATIC_DRAW); // sizeof(glm::vec3) == 12
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)nullptr);
-    glEnableVertexAttribArray(0);
-
-    // surface normal
-    glGenBuffers(1, &m_vbo_normals);
-    glBindBuffer(GL_ARRAY_BUFFER, m_vbo_normals);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * normals.size(), normals.data(), GL_STATIC_DRAW);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)nullptr);
-    glEnableVertexAttribArray(1);
-
-    glBindVertexArray(0);
+models_interface_type RenderSystem::load_models(std::unordered_map<std::string, std::string> models) {
+    return m_model_manager.load_models(models);
 }
 
+cubemaps_interface_type RenderSystem::load_cubemaps(std::unordered_map<std::string, CubemapFaces> cubemaps) {
+    return m_texture_manager.load_cubemaps(cubemaps);
+}
+
+void RenderSystem::init(Entity camera) {
+    // setup opengl properties
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+
+    // add window resize listener
+    ref_scene.add_event_listener(METHOD_LISTENER(Events::Window::RESIZED, RenderSystem::window_size_listener));
+    
+    // initialize shaders
+    shader = std::make_unique<Shader>(std::string(FS_SHADERS_DIR) + "vertex.vs", std::string(FS_SHADERS_DIR) + "fragment.fs");
+    model_shader = std::make_unique<Shader>(std::string(FS_SHADERS_DIR) + "shader_model.vs", std::string(FS_SHADERS_DIR) + "shader_model.fs");
+    cubemap_shader = std::make_unique<Shader>(std::string(FS_SHADERS_DIR) + "cubemap.vs", std::string(FS_SHADERS_DIR) + "cubemap.fs");
+
+    m_camera = camera;
+}
+
+void RenderSystem::draw_cubemap(unsigned int cubemap_id) {
+    auto& camera_transform = ref_scene.get_component<Transform>(m_camera);
+    auto& camera = ref_scene.get_component<Camera>(m_camera);
+
+    m_texture_manager.draw_cubemap(cubemap_id, *cubemap_shader.get(), Camera::create_view_matrix(camera_transform.position), camera.projection_transform);
+}
 
 void RenderSystem::update(float dt)
 {
     // clear screen and buffers
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // NOLINT (hicpp-signed-bitwise)
-
-    shader->activate();
-    glBindVertexArray(m_vao);
 
     auto& camera_transform = ref_scene.get_component<Transform>(m_camera);
     auto& camera = ref_scene.get_component<Camera>(m_camera);
 
-    // loop through all entities in RenderSystem
-    for (const auto& entity : SceneView<Renderable, Transform>(ref_scene))
-    {
-        const auto& transform = ref_scene.get_component<Transform>(entity);
-        const auto& renderable = ref_scene.get_component<Renderable>(entity);
+    // // loop through all entities in RenderSystem
+    // for (const auto& entity : SceneView<Renderable, Transform>(ref_scene))
+    // {
+    //     const auto& transform = ref_scene.get_component<Transform>(entity);
+    //     const auto& renderable = ref_scene.get_component<Renderable>(entity);
+        
+    //     glm::mat4 model = glm::mat4(1.0);
+
+    //     model = glm::translate(model, transform.position);
+    //     model = glm::rotate(model, (transform.rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+    //     model = glm::rotate(model, (transform.rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+    //     model = glm::rotate(model, (transform.rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
+    //     model = glm::scale(model, transform.scale);
+        
+    //     // camera.view_matrix should be created just before rendering,
+    //     // as camera position keeps changing
+    //     camera.view_matrix = Camera::create_view_matrix(camera_transform.position); 
+
+    //     // set uniforms
+    //     shader->set_uniform<glm::mat4>("uModel", model);
+    //     shader->set_uniform<glm::mat4>("uView", camera.view_matrix);
+    //     shader->set_uniform<glm::mat4>("uProjection", camera.projection_transform);
+    //     shader->set_uniform<glm::vec3>("uColor", renderable.color);
+
+    //     // draw cube to screen
+    //     glDrawArrays(GL_TRIANGLES, 0, 36);
+    // }
+
+    // // // unbind VAO
+    // glBindVertexArray(0);
+
+    // draw cubemap
+    // for(const auto& entity : SceneView<Renderable, Cubemap>(ref_scene)) {
+    //     auto& cubemap = ref_scene.get_component<Cubemap>(entity);
+    //     m_texture_manager.draw_cubemap(cubemap.id);
+    // }
+
+    /// draw all models
+
+    glEnable(GL_DEPTH_TEST);
+    model_shader->activate();
+
+    for(const auto& entity : SceneView<Renderable, Model, Transform>(ref_scene)) {
+        auto& transform = ref_scene.get_component<Transform>(entity);
+        auto& object_model = ref_scene.get_component<Model>(entity);
+        // const auto& renderable = ref_scene.get_component<Renderable>(entity);
         
         glm::mat4 model = glm::mat4(1.0);
 
@@ -152,23 +111,25 @@ void RenderSystem::update(float dt)
         model = glm::rotate(model, (transform.rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::rotate(model, (transform.rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
         model = glm::scale(model, transform.scale);
-        
+
         // camera.view_matrix should be created just before rendering,
         // as camera position keeps changing
         camera.view_matrix = Camera::create_view_matrix(camera_transform.position); 
 
         // set uniforms
-        shader->set_uniform<glm::mat4>("uModel", model);
-        shader->set_uniform<glm::mat4>("uView", camera.view_matrix);
-        shader->set_uniform<glm::mat4>("uProjection", camera.projection_transform);
-        shader->set_uniform<glm::vec3>("uColor", renderable.color);
+        model_shader->set_uniform<glm::mat4>("model", model);
+        model_shader->set_uniform<glm::mat4>("view", camera.view_matrix);
+        model_shader->set_uniform<glm::mat4>("projection", camera.projection_transform);
+        // shader->set_uniform<glm::vec3>("uColor", renderable.color);
 
-        // draw cube to screen
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        m_model_manager.draw_model(model_shader, object_model.model_id);
+        // object_model.model.Draw(model_shader); // modelloader::draw itself must activate shader before drawing
     }
 
-    // unbind VAO
-    glBindVertexArray(0);
+    // draw cubemaps
+    for(const auto& entity : SceneView<Renderable, Cubemap>(ref_scene)) {
+        draw_cubemap(ref_scene.get_component<Cubemap>(entity).id);
+    }
 }
 
 void RenderSystem::window_size_listener(Event& event)
@@ -177,5 +138,5 @@ void RenderSystem::window_size_listener(Event& event)
     auto window_height = event.get_param<unsigned int>(Events::Window::Resized::HEIGHT);
 
     auto& camera = ref_scene.get_component<Camera>(m_camera);
-    camera.projection_transform = Camera::create_projection_transform(45.0f, 0.1f, 1000.0f, window_width, window_height);
+    camera.projection_transform = Camera::create_projection_transform(45.0f, 0.1f, 1000.0f, window_width, window_height); // TODO: set appropriate far plane without clipping too early
 }
--- src/engine/ecs/systems/RenderSystem.hpp
+++ src/engine/ecs/systems/RenderSystem.hpp
@@ -4,24 +4,34 @@
 
 #include <engine/ecs/core/System.hpp>
 #include <engine/ecs/core/Event.hpp>
+#include <engine/graphics/ModelManager.hpp>
+#include <engine/graphics/TextureManager.hpp>
 
 #include <engine/graphics/Shader.hpp>
 
 class RenderSystem : public System
 {
 public:
-    RenderSystem(Scene& scene): System(scene) {}
+    RenderSystem(Scene& scene): System(scene), m_model_manager(m_texture_manager, std::string(FS_RESOURCES_DIR) + "models/bin") {}
 
     void init(Entity camera);
     void update(float dt);
+
+    models_interface_type load_models(std::unordered_map<std::string, std::string> models);
+
+    cubemaps_interface_type load_cubemaps(std::unordered_map<std::string, CubemapFaces> cubemaps);
+    void draw_cubemap(unsigned int cubemap_id);
 private:
     void window_size_listener(Event& event);
 
+    // FIX: prefix shader names by m_
     std::unique_ptr<Shader> shader;
-
+    std::unique_ptr<Shader> model_shader;
+    std::unique_ptr<Shader> cubemap_shader;
+    
+    // FIX: create a Camera class which wraps around camera entity
     Entity m_camera;
 
-    GLuint m_vao{};
-    GLuint m_vbo_vertices{};
-    GLuint m_vbo_normals{};
+    ModelManager m_model_manager;
+    TextureManager m_texture_manager;
 };
