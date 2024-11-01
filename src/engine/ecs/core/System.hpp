2f158e8d8ac5ee91855f3329aa04fa963cfc44d8
cmd05 <splendid.snippet670@slmail.me>
2024-11-01 09:52:48 +0530

    add TODO: use Scene pointer instead of reference

--- src/engine/ecs/core/System.hpp
+++ src/engine/ecs/core/System.hpp
@@ -11,5 +11,6 @@ class System
 public:
     System(Scene& scene) : ref_scene(scene) {}
 protected:
+    // TODO: use scene pointer `Scene* m_scene` and modify for all systems
     Scene& ref_scene;
 };
