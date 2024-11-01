71c3651ef1406c43d4ac82c7dfbeb79efbfdc130
cmd05 <splendid.snippet670@slmail.me>
2024-11-01 22:00:22 +0530

    add light rendering

--- src/engine/ecs/systems/RenderSystem.cpp
+++ src/engine/ecs/systems/RenderSystem.cpp
@@ -14,8 +14,10 @@
 #include <engine/ecs/components/Renderable.hpp>
 #include <engine/ecs/components/Transform.hpp>
 #include <engine/ecs/components/Model.hpp>
+#include <engine/ecs/components/PointLightComponent.hpp>
 
 #include <engine/graphics/Shader.hpp>
+#include <engine/graphics/objects/CubeObject.hpp>
 
 RenderSystem::RenderSystem(Scene& scene, Entity camera): 
     System(scene),
@@ -26,8 +28,11 @@ RenderSystem::RenderSystem(Scene& scene, Entity camera):
     glEnable(GL_DEPTH_TEST);
 
     // add window resize listener
-    ref_scene.add_event_listener(METHOD_LISTENER(Events::Window::RESIZED, RenderSystem::window_size_listener));
     
+    // build default objects and their VBO's
+    // this should happen only after opengl context is setup
+    g_graphics_objects.init();
+
     // initialize shaders
     model_shader = std::make_unique<Shader>(std::string(FS_SHADERS_DIR) + "shader_model.vs", std::string(FS_SHADERS_DIR) + "shader_model.fs");
     cubemap_shader = std::make_unique<Shader>(std::string(FS_SHADERS_DIR) + "cubemap.vs", std::string(FS_SHADERS_DIR) + "cubemap.fs");
@@ -52,20 +57,44 @@ void RenderSystem::draw_cubemap(unsigned int cubemap_id) {
 
 void RenderSystem::update(float dt)
 {
+    // TODO: check the ordering of all opengl calls and system updates according to main.cpp and render loop
+
     // clear screen and buffers
     glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // draw all models
+    // set OpenGL parameters
     glEnable(GL_DEPTH_TEST);
-    model_shader->activate();
+    // glEnable(GL_CULL_FACE);
 
+    model_shader->activate();
+    model_shader->set_uniform<glm::vec3>("viewPos", m_camera_wrapper.get_transform_component().position);
+    
+    int i_lights = 0;
+    for(const auto& entity : SceneView<PointLightComponent, Transform>(ref_scene)) {
+        glm::vec3 light_color = ref_scene.get_component<PointLightComponent>(entity).light_color;
+        Transform light_transform = ref_scene.get_component<Transform>(entity);
+
+        m_light_renderer->draw_light_cube(light_transform, m_camera_wrapper, light_color);
+
+        model_shader->activate();
+
+        // TODO: fix all uniforms to use layout= instead of glGetUniform. This will require adding a new method to Shader.cpp
+        model_shader->set_uniform<glm::vec3>("pointLights[" + std::to_string(i_lights) + "].position", light_transform.position);
+        model_shader->set_uniform<glm::vec3>("pointLights[" + std::to_string(i_lights) + "].color", light_color);
+        
+        i_lights++;
+    }
+
+    // draw models
     for(const auto& entity : SceneView<Renderable, Model, Transform>(ref_scene)) {
         const auto& transform = ref_scene.get_component<Transform>(entity);
         const auto& object_model = ref_scene.get_component<Model>(entity);
 
         m_model_manager.draw_model(model_shader, object_model.model_id, m_camera_wrapper, transform);
     }
+
+    // glDisable(GL_CULL_FACE);
 
     // draw cubemaps
     for(const auto& entity : SceneView<Renderable, Cubemap>(ref_scene))
