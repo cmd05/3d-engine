ae324a7a4b46810296a7d6f55528ff76e0ccd1d0
cmd05 <splendid.snippet670@slmail.me>
2024-11-02 08:13:57 +0530

    add MVP matrices helper functions

--- src/engine/graphics/lib/GraphicsHelper.hpp
+++ src/engine/graphics/lib/GraphicsHelper.hpp
@@ -5,6 +5,9 @@
 
 #include <engine/ecs/components/Transform.hpp>
 
+#include <engine/graphics/CameraWrapper.hpp>
+#include <engine/graphics/Shader.hpp>
+
 constexpr glm::vec3 MATH_X_AXIS {1.0f, 0.0f, 0.0f};
 constexpr glm::vec3 MATH_Y_AXIS {0.0f, 1.0f, 0.0f};
 constexpr glm::vec3 MATH_Z_AXIS {0.0f, 0.0f, 1.0f};
@@ -21,5 +24,35 @@ public:
         glm::mat4 model = translation_mat * rot_mat * scale_mat;
 
         return model;
+    }
+
+    struct MVP {
+        glm::mat4 model;
+        glm::mat4 view;
+        glm::mat4 projection;
+    };
+
+    static MVP get_mvp(const Transform& transform, const CameraWrapper& camera_wrapper) {
+        MVP mvp;
+
+        mvp.model = create_model_matrix(transform);
+        mvp.view = camera_wrapper.get_view_matrix();
+        mvp.projection = camera_wrapper.get_projection_matrix();
+
+        return mvp;
+    }
+
+    static void set_mvp(const std::unique_ptr<Shader>& shader, const Transform& transform, const CameraWrapper& camera_wrapper) {
+        MVP mvp = get_mvp(transform, camera_wrapper);
+        set_mvp(shader, mvp);
+    }
+
+    static void set_mvp(const std::unique_ptr<Shader>& shader, MVP mvp, 
+        const std::string model_uniform = "model", const std::string view_uniform = "view", const std::string projection_uniform = "projection") {
+        shader->activate();
+
+        shader->set_uniform<glm::mat4>(model_uniform, mvp.model);
+        shader->set_uniform<glm::mat4>(view_uniform, mvp.view);
+        shader->set_uniform<glm::mat4>(projection_uniform, mvp.projection);
     }
 };
