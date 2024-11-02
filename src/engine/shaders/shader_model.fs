9949b70f49e0e7dad776d07d85160d58c8ca6bda
cmd05 <splendid.snippet670@slmail.me>
2024-11-02 08:38:32 +0530

    updated model shaders. - blinn phong lighting - loading specular maps - attenuation - gamma correction

--- src/engine/shaders/shader_model.fs
+++ src/engine/shaders/shader_model.fs
@@ -1,12 +1,104 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoords;
+in VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+} fs_in;
 
 uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_specular1;
 
-void main()
-{    
-    FragColor = texture(texture_diffuse1, TexCoords);
-    // FragColor = vec4(vec3(gl_FragDepth), 1.0f);
+uniform bool ambient_tex_exists;
+uniform bool diffuse_tex_exists;
+uniform bool specular_tex_exists;
+// normal, metallic, roughness ...
+
+uniform vec3 lightPos;
+uniform vec3 lightColor;
+uniform vec3 viewPos;
+
+struct Light {
+    vec3 position;
+    vec3 color;
+};
+
+#define NR_POINT_LIGHTS 1
+
+uniform Light point_lights[NR_POINT_LIGHTS];
+
+const float att_const = 1.0;
+const float att_linear = 0.00009;
+const float att_quadratic = 0.000032;
+
+// void main()
+// {    
+//     FragColor = texture(texture_diffuse1, TexCoords);
+//     // FragColor = vec4(vec3(gl_FragDepth), 1.0f);
+// }
+
+void main() {
+    vec3 color = texture(texture_diffuse1, fs_in.TexCoords).rgb;
+    // FragColor = vec4(color, 1.0);
+
+    // // ambient
+    vec3 ambient = 0.05 * color;
+
+    // diffuse
+    // vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+    vec3 normal = normalize(fs_in.Normal);
+
+    // float diff = max(dot(lightDir, normal), 0.0);
+    // vec3 diffuse = diff * color;
+
+    // specular
+    vec3 viewDir = normalize(viewPos - fs_in.FragPos);
+    // vec3 reflectDir = reflect(-lightDir, normal);
+    // float spec = 0.0;
+
+    // vec3 halfwayDir = normalize(lightDir + viewDir);
+    // spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
+
+    // vec3 specular = vec3(0.1) * spec;
+    // vec3 result = (ambient + diffuse + specular) * lightColor;
+
+    vec3 result = vec3(0.0);
+
+    for(int i = 0; i < NR_POINT_LIGHTS; i++) {
+        vec3 lightDir = normalize(point_lights[i].position - fs_in.FragPos);
+        
+        float diff = max(dot(lightDir, normal), 0.0);
+        vec3 diffuse = diff * color;
+
+        // vec3 reflectDir = reflect(-lightDir, normal);
+        
+        vec3 halfwayDir = normalize(lightDir + viewDir);
+        float spec = pow(max(dot(normal, halfwayDir), 0.0), 256.0);
+
+        // 0.7, 32.0 for seeing clear specular reflections
+        vec3 specular = vec3(0.3) * spec;
+
+        // orange specular => no specular texture for model/mesh
+        if(specular_tex_exists)
+            specular *= vec3(texture(texture_specular1, fs_in.TexCoords));
+        else
+            specular = vec3(0.0);
+        
+        float dist = length(point_lights[i].position - fs_in.FragPos);
+        float attenuation = 1.0 / (att_const + att_linear * dist + att_quadratic * (dist * dist));
+
+        // note: attenuation won't apply to ambient component
+        diffuse *= attenuation;
+        specular *= attenuation;
+
+        vec3 component = (ambient + diffuse + specular) * point_lights[i].color;
+
+        result += component;
+    }
+
+    // if(gamma)
+    result = pow(result, vec3(1.0/2.2)); // do gamma correction
+
+    FragColor = vec4(vec3(result), 1.0);
 }
--- src/engine/shaders/shader_model.vs
+++ src/engine/shaders/shader_model.vs
@@ -3,14 +3,44 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
 
-out vec2 TexCoords;
+out VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+} vs_out;
+
+// ---
+
+// #define NR_POINT_LIGHTS 1
+
+// uniform vec3 viewPos;
+
+// struct Light {
+//     vec3 position;
+//     vec3 color;
+// };
+
+// out VS_OUT {
+//     vec3 TangentFragPos;
+//     vec2 TexCoords;
+//     vec3 TangentViewPos;
+//     uniform Light TangentSpace_point_lights[NR_POINT_LIGHTS];
+// } vs_out;
+
+// ---
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-void main()
-{
-    TexCoords = aTexCoords;    
+void main() {
+    vs_out.FragPos = vec3(model * vec4(aPos, 1.0));
+    // vs_out.Normal = aNormal;
+
+    mat3 normalMatrix = transpose(inverse(mat3(model)));
+    vs_out.Normal = normalize(normalMatrix * aNormal);
+
+    vs_out.TexCoords = aTexCoords;
+
     gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
