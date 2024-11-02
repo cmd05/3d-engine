f9cd1ef43ec24a9919ad17589a67f81ff69d0c64
cmd05 <splendid.snippet670@slmail.me>
2024-11-02 08:36:43 +0530

    create possible structure for Application

--- src/application/Application.hpp
+++ src/application/Application.hpp
@@ -1,3 +1,36 @@
+#include <engine/ecs/core/Scene.hpp>
+#include <engine/window/WindowManager.hpp>
+#include <engine/GUI/GUIMain.hpp>
+
 // TODO: create an application class
 // Application::init()
 // Application::run()
+
+class Application {
+    // avoid coupled references to each other
+    Scene main_scene;
+    InputHandler input_handler; // not defined as a member of `Scene`
+    WindowManager window_manager {main_scene, input_handler};
+    GUIMain gui_layer {window_manager, "#version 130"};
+
+    // systems
+    // maybe Scene::register_system should return pointer instead of reference
+    // so that we can have PhysicsSystem* m_physics_system
+    
+public:
+    void init();
+    void update();
+
+    void run(); // run() internally calls update
+};
+
+/**
+
+int main() {
+    Application app; //
+    app.init();
+
+    app.run();
+}
+
+ */
