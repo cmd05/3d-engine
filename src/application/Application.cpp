ddb990be4cdfb2bbb8ad1f709e607ead1ce06ea5
cmd05 <splendid.snippet670@slmail.me>
2025-04-10 23:12:39 +0530

    feat: use `Application` in main file

---
+++ src/application/Application.cpp
@@ -0,0 +1,190 @@
+#include <engine/config/WindowConfig.hpp>
+#include <engine/config/Events.hpp>
+#include <application/Application.hpp>
+#include <engine/ecs/core/Types.hpp>
+#include <engine/ecs/core/Event.hpp>
+
+#include <engine/ecs/components/Cubemap.hpp>
+#include <engine/ecs/components/Camera.hpp>
+#include <engine/ecs/components/Gravity.hpp>
+#include <engine/ecs/components/Player.hpp>
+#include <engine/ecs/components/Renderable.hpp>
+#include <engine/ecs/components/RigidBody.hpp>
+#include <engine/ecs/components/Thrust.hpp>
+#include <engine/ecs/components/Transform.hpp>
+#include <engine/ecs/components/Model.hpp>
+#include <engine/ecs/components/PointLight.hpp>
+
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+Application::Application() {
+    register_callbacks();
+
+    m_input_handler = std::make_unique<InputHandler>(m_main_scene);
+    m_window_manager = std::make_unique<WindowManager>(m_main_scene, *m_input_handler);
+    m_gui_state = std::make_unique<GUIState>();
+    m_gui_main = std::make_unique<GUIMain>(*m_gui_state, m_glsl_version);
+
+    int window_width = WindowConfig::DEFAULT_SCR_WIDTH;
+    int window_height = WindowConfig::DEFAULT_SCR_HEIGHT;
+
+    m_window_manager->bind_gui(*m_gui_main);
+    m_window_manager->init("3D Engine", window_width, window_height, 0, 0);
+    
+    m_gui_main->bind_window_manager(*m_window_manager);
+    m_gui_main->init("3D Engine Editor");
+
+    register_ecs_components();
+    register_ecs_systems();
+}
+
+void Application::init() {
+    load_models();
+
+    init_lights();
+    init_models();
+    m_render_system->set_uniforms_pre_rendering();
+}
+
+void Application::load_models() {
+    try {
+        std::unordered_map<std::string, std::string> models;
+
+        std::ifstream models_ifs {std::string(FS_RESOURCES_DIR) + "models.json"};
+        nlohmann::json j_models;
+        models_ifs >> j_models;
+
+        if (j_models.contains("models") && j_models["models"].is_array()) {
+            for (const auto& obj : j_models["models"]) {
+                if (!obj.empty()) {
+                    auto it = obj.begin();
+                    std::string key = it.key();
+                    std::string value = it.value();
+                    
+                    models[key] = std::string(FS_RESOURCES_DIR) + value;
+                }
+            }
+        }
+
+        m_models_map = m_render_system->load_models(models);
+    } catch(...) {
+        ASSERT_MESSAGE("Error importing models. Check debug");
+    }
+}
+
+void Application::init_lights() {
+    const int NR_LIGHTS = 1;
+    std::vector<Entity> light_entities;
+
+    for(int i = 0; i < NR_LIGHTS; i++) {
+        auto light_entity = m_main_scene.create_entity();
+
+        m_main_scene.add_components(light_entity,
+            Components::Transform {
+                .position = glm::vec3(10.0f, 50.0f, 10.0f),
+                .scale = glm::vec3(5.0f)
+            },
+
+            Components::PointLight {
+                .light_color = glm::vec3(1.0f)
+            }
+        );
+    }
+}
+
+void Application::init_models() {
+    for(auto& [_, v] : m_models_map) {
+        auto model_entity = m_main_scene.create_entity();
+
+        m_main_scene.add_components(model_entity,
+            Components::Transform {
+                .position = glm::vec3(0.0f),
+                .scale = glm::vec3(0.2f)
+            },
+            Components::Renderable{},
+            Components::Model{.model_id = v }
+        );
+    }
+}
+
+void Application::run() {
+    float last_frame, current_frame;
+    last_frame = current_frame = m_window_manager->get_time(); // windowManager.get_time() is not exactly zero at this call
+    float dt = 0.0f;
+
+    while (!m_quit) {
+        m_window_manager->process_events(); // first process window events
+
+        m_camera_control_system->update(dt);
+        m_render_system->update(dt);
+
+        m_gui_main->new_frame();
+        m_gui_main->update();
+        m_gui_main->render();
+        m_gui_main->update_platform_windows();
+
+        m_window_manager->update(); // window updation happens after windows have been processed
+
+        update_frame_times(m_window_manager->get_time(), current_frame, last_frame, dt);
+    }
+}
+
+void Application::register_callbacks() {
+    m_main_scene.add_event_listener(METHOD_LISTENER(Events::Window::QUIT, Application::quit_handler));
+    
+    // initialize program state (ex: g_graphics_objects) once OpenGL has been initialized
+    m_main_scene.add_event_listener(FUNCTION_LISTENER(Events::Window::GL_INIT, RenderSystem::gl_init_callback));
+}
+
+void Application::init_camera(int width, int height) {
+    m_camera_entity = m_main_scene.create_entity();
+
+    m_main_scene.add_components(
+        m_camera_entity,
+        Components::Transform {.position = GraphicsConfig::Camera::DEFAULT_CAM_POS},
+        Components::Camera(width, height)
+    );
+}
+
+void Application::register_ecs_systems() {
+    m_physics_system = &m_main_scene.register_system<PhysicsSystem>();
+    m_camera_control_system = &m_main_scene.register_system<CameraControlSystem>(*m_input_handler);
+ 
+    // set render system
+    int win_framebuffer_width, win_framebuffer_height;
+    m_window_manager->get_framebuffer_size(win_framebuffer_width, win_framebuffer_height);
+    ENGINE_LOG(win_framebuffer_width << " " << win_framebuffer_height);
+
+    init_camera(win_framebuffer_width, win_framebuffer_height);
+
+    m_render_system = &m_main_scene.register_system<RenderSystem>(m_camera_entity, *m_gui_state);
+    m_render_system->init_framebuffer_size(win_framebuffer_width, win_framebuffer_height);
+}
+
+void Application::register_ecs_components() {
+    m_main_scene.register_component<
+        Components::Camera,
+        Components::Gravity,
+        Components::Renderable,
+        Components::RigidBody,
+        Components::Transform,
+        Components::Model,
+        Components::Cubemap,
+        Components::PointLight,
+        
+        Components::Player,
+        Components::Thrust
+    >();
+}
+
+void Application::update_frame_times(float new_time, float &curr_time, float &last_time, float &dt) {
+    curr_time = new_time;
+    dt = curr_time - last_time;
+    last_time = curr_time;
+}
+
+void Application::quit_handler(Event &event) {
+    ENGINE_LOG("[quit_handler]");
+    m_quit = true;
+}
--- src/application/Application.hpp
+++ src/application/Application.hpp
@@ -2,25 +2,58 @@
 #include <engine/window/WindowManager.hpp>
 #include <engine/gui/GUIMain.hpp>
 
+#include <engine/ecs/systems/CameraControlSystem.hpp>
+#include <engine/ecs/systems/PhysicsSystem.hpp>
+#include <engine/ecs/systems/PlayerControlSystem.hpp>
+#include <engine/ecs/systems/RenderSystem.hpp>
+
+#include <memory>
 // Application::init()
 // Application::run()
 
 class Application {
-    // avoid coupled references to each other
-    Scene main_scene;
-    InputHandler input_handler; // not defined as a member of `Scene`
-    WindowManager window_manager {main_scene, input_handler};
-    GUIMain gui_layer {window_manager, "#version 130"};
+public:
+    Application();
+    void init();
+
+    void load_models();
+    void init_lights();
+    void init_models();
 
     // systems
     // maybe Scene::register_system should return pointer instead of reference
     // so that we can have PhysicsSystem* m_physics_system
     
-public:
-    void init();
+    // void init();
     void update();
+    
+    void run(); // runs till window is not closed
+private:
+    void register_callbacks();
+    void register_ecs_components();
+    void init_camera(int width, int height);
+    void register_ecs_systems(); // automatically creates main camera entity
 
-    void run(); // run() internally calls update
+    void update_frame_times(float new_time, float& curr_time, float& last_time, float& dt);
+    void quit_handler(Event& event);
+private:
+    Scene m_main_scene;
+    std::unique_ptr<InputHandler> m_input_handler;
+    std::unique_ptr<WindowManager> m_window_manager;
+    std::unique_ptr<GUIMain> m_gui_main;
+    std::unique_ptr<GUIState> m_gui_state;
+
+    // Scene owns the systems
+    PhysicsSystem* m_physics_system;
+    CameraControlSystem* m_camera_control_system;
+    RenderSystem* m_render_system;
+
+    models_interface_type m_models_map;
+    
+    Entity m_camera_entity; // primary camera to be used for rendering the main scene
+
+    const char* m_glsl_version = "#version 130";
+    bool m_quit = false;
 };
 
 /**
